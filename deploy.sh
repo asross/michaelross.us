9013a1f3934d4247eb1d114f5b86d15d105de3c2
Andrew Ross <andrew.ross@arcadia.com>
2022-10-06 18:28:41 -0400

    redundancy

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll build && aws s3 sync _site/ s3://michaelross.us --delete --acl public-read
+bundle exec jekyll build && aws s3 sync _site/ s3://michaelross.us --delete --acl public-read && aws s3 sync _site/ s3://www.michaelross.us --delete --acl public-read
